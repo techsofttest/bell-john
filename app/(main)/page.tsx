c525d6e25f8fdc70ec24c01f728126a3626f8af3
techsofttest <techsofttest@gmail.com>
2026-04-30 18:37:16 +0530

    chore: ensure home page move is tracked

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,12 +1,12 @@
-import HeroSlider from "./components/global/HeroSlider";
-import FeaturedProducts from "./components/global/FeaturedProducts";
-import CoreCategories from "./components/global/CoreCategories";
-import PromoBanner from "./components/global/PromoBanner";
-import CategorySpotlight from "./components/global/CategorySpotlight";
-import VisionMission from "./components/global/VisionMission";
-import ValueProposition from "./components/global/ValueProposition";
-import ContactSection from "./components/global/ContactSection";
-import MarqueeStrip from "./components/global/MarqueeStrip";
+import HeroSlider from "../components/global/HeroSlider";
+import FeaturedProducts from "../components/global/FeaturedProducts";
+import CoreCategories from "../components/global/CoreCategories";
+import PromoBanner from "../components/global/PromoBanner";
+import CategorySpotlight from "../components/global/CategorySpotlight";
+import VisionMission from "../components/global/VisionMission";
+import ValueProposition from "../components/global/ValueProposition";
+import ContactSection from "../components/global/ContactSection";
+import MarqueeStrip from "../components/global/MarqueeStrip";
 
 // Mock Offers for the Home Page Promo Banner
 const mockOffers = [
